285786a84d90c2163db2590323a30823db6adbc3
Himanshu Kumar <himanshukr033@gmail.com>
2024-05-15 14:51:26 +0530

    deploy bug fixes

--- Backend/index.js
+++ Backend/index.js
@@ -28,23 +28,23 @@ app.use('/api/message', messageRoutes)
 
 
 
-// --------------------------deployment------------------------------
+// // --------------------------deployment------------------------------
 
-const __dirname1 = path.resolve();
+// const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/Frontend/chat-app/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname1, "/Frontend/chat-app/dist")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "Frontend", "chat-app","dist", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname1, "Frontend", "chat-app","dist", "index.html"))
+//   );
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running..");
+//   });
+// }
 
-// --------------------------deployment------------------------------
+// // --------------------------deployment------------------------------
 
 
 
